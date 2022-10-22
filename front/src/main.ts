2ad45a6df918bc2c6b25e297cad21ddb8733298f
Afanasev Alexander <afanasev.a@openregion.info>
2022-10-23 01:21:02 +0400

    Element plus

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,7 +1,12 @@
 import '@/assets/styles/style.scss'
+import 'element-plus/dist/index.css'
 
+import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 
 import App from '@/App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+
+app.mount('#app')
